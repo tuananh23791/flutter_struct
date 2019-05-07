2f794673af0e067f44de0793ca854d58794ec136
Hoa Nguyen <hoa.nguyenminh.it@gmail.com>
2019-05-07 12:20:53 +0700

    multi barcode temp1

--- app/src/main/java/com/beyondedge/hm/ui/screen/ScanActivity.java
+++ app/src/main/java/com/beyondedge/hm/ui/screen/ScanActivity.java
@@ -1,21 +1,30 @@
 package com.beyondedge.hm.ui.screen;
 
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.beyondedge.hm.R;
 import com.beyondedge.hm.base.BaseActivity;
+import com.beyondedge.zxingscanner.ZXingScannerViewCustom;
 import com.google.zxing.Result;
 
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 /**
  * Created by Hoa Nguyen on Apr 25 2019.
  */
-public class ScanActivity extends BaseActivity implements ZXingScannerView.ResultHandler {
+public class ScanActivity extends BaseActivity implements ZXingScannerViewCustom.ResultHandler {
 
-    private ZXingScannerView mScannerView;
+    int numScan = 0;
+    private ZXingScannerViewCustom mScannerView;
+    private TextView barcode;
+    private ScrollView scrollView;
     private ImageView btFlash;
     private boolean mFlash = false;
 
@@ -27,8 +36,10 @@ public class ScanActivity extends BaseActivity implements ZXingScannerView.Resul
         enableBackButtonToolbar();
 
         ViewGroup contentFrame = findViewById(R.id.content_frame);
+        barcode = findViewById(R.id.barcode);
+        scrollView = findViewById(R.id.scrollView);
         btFlash = findViewById(R.id.btFlash);
-        mScannerView = new ZXingScannerView(this);
+        mScannerView = new ZXingScannerViewCustom(this);
         contentFrame.addView(mScannerView);
         mScannerView.setFlash(mFlash);
 
@@ -69,8 +80,25 @@ public class ScanActivity extends BaseActivity implements ZXingScannerView.Resul
 //        Handler handler = new Handler();
 //        handler.postDelayed(() -> mScannerView.resumeCameraPreview(ScanActivity.this), 2000);
 
-        showMessageDialog("Contents = " + rawResult.getText() +
-                ", Format = " + rawResult.getBarcodeFormat().toString());
+//        showMessageDialog("Contents = " + rawResult.getText() +
+//                ", Format = " + rawResult.getBarcodeFormat().toString());
+
+        StringBuilder builder = new StringBuilder(barcode.getText());
+        builder.append("\n ")
+                .append(++numScan)
+                .append(" ---------------------------")
+                .append("\n")
+                .append(rawResult.getText())
+                .append("\nEnd ---------------------------\n");
+        barcode.setText(builder.toString());
+
+        ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+        toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 250);
+
+        scrollView.post(() -> scrollView.fullScroll(View.FOCUS_DOWN));
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> mScannerView.resumeCameraPreview(ScanActivity.this), 1000);
     }
 
     public void showMessageDialog(String message) {
