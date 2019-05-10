e03a201073df6a01f7fc6ad4106f334c59136444
Hoa Nguyen <hoa.nguyenminh.it@gmail.com>
2019-05-10 11:20:41 +0700

    kt convert 2

--- app/src/main/java/com/beyondedge/hm/config/Constant.java
+++ app/src/main/java/com/beyondedge/hm/config/Constant.java
@@ -14,7 +14,7 @@ import com.beyondedge.hm.utils.PrefManager;
  */
 public class Constant {
     /*Cheat CODE*/
-    public static final boolean IS_FORCE_LOCAL_CONFIG = false;
+    public static final boolean IS_FORCE_LOCAL_CONFIG = true;
     public static final boolean IS_FORCE_LOCAL_THAI_CONFIG = false;
 
     public static final String LINK_CONFIG = "http://sharefile.beyondedge.com.sg/hm/setting/ID-EN.txt";
